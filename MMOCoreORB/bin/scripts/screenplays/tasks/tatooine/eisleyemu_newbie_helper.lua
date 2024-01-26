6ee7bc4e4fce8c6352acfdae0608f42b40414fa8
Kevin Schmidt <kevin@kswebentwicklung.de>
2024-01-26 10:09:44 +0100

    c3po model for newbie helper

--- MMOCoreORB/bin/scripts/screenplays/tasks/tatooine/eisleyemu_newbie_helper.lua
+++ MMOCoreORB/bin/scripts/screenplays/tasks/tatooine/eisleyemu_newbie_helper.lua
@@ -36,24 +36,10 @@ function eisleyemu_newbie_helper_conversation_handler:getNextConversationScreen(
                 -- Set variable to track what option the player picked and get the option picked                
                 local optionLink = luaLastConversationScreen:getOptionLink(selectedOption)
                 nextConversationScreen = conversation:getScreen(optionLink)
-                -- Get some information about the player.
-                -- local credits = creature:getCashCredits()
-                -- local pInventory = creature:getSlottedObject("inventory")
-                -- local inventory = LuaSceneObject(pInventory)
-                -- Take action when the player makes a purchase.
-                --if (inventory:hasFullContainerObjects() == true) then -- removed, does not work
-                -- if (SceneObject(pInventory):isContainerFullRecursive()) then
-                    -- Bail if the player doesn’t have enough space in their inventory.
-                    -- Plays a chat box message from the NPC as well as a system message.
-                    -- nextConversationScreen = conversation:getScreen("insufficient_space")
-                    -- creature:sendSystemMessage("You do not have enough inventory space")
-               -- elseif (optionLink == "speederbike" and credits < 10000) then
-                    -- Bail if the player doesn’t have enough cash on hand.  
-                    -- Plays a chat box message from the NPC as well as a system message.
-                      -- nextConversationScreen = conversation:getScreen("insufficient_funds")
-                      -- creature:sendSystemMessage("You have insufficient funds") 
+                -- Further conversation dialogs
                 if (optionLink == "server") then
                     nextConversationScreen = conversation:getScreen("server")
+                -- elseif (optionLink == "speederbike") then
                 end
             end
         end
